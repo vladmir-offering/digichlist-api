92c8abd301733211316cfcd08567d993460f28b8
olehyaremyn7 <oleh_yaremyn@ukr.net>
2021-01-30 16:38:20 +0200

    remove shortid

--- models/User.js
+++ models/User.js
@@ -1,11 +1,6 @@
 const { Schema, model } = require('mongoose');
-const shortid = require('shortid');
 
 const userSchema = new Schema({
-    _id: {
-        type: String,
-        default: shortid.generate,
-    },
     first_name: {
         type: String,
         default: '',
